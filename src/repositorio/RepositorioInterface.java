c5e3f2ac77f9f9f9e67163dd0164ffcabd5947f7
Luciano <lucianoalves413@gmail.com>
2018-07-16 16:14:54 -0300

    Criado estoque

--- src/repositorio/RepositorioInterface.java
+++ src/repositorio/RepositorioInterface.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public interface RepositorioInterface <T>{
 
+    public int recuperarIndice(String t);
     public void cadastrar(T t);
-    public T recuperar(String x);
+    public T recuperar(String t);
     public void remover(T t);
     public void atualizar(String x, T t);
     public ArrayList<T> recuoertarTudo();
